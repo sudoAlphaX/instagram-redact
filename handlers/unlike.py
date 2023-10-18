e44df050f29f08875d3f0c3235c530557d05645a
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-10-18 23:47:46 +0530

    add rate limit error handling

--- handlers/unlike.py
+++ handlers/unlike.py
@@ -1,5 +1,8 @@
 from client import cl, clientlogger
 from helpers.logutils import joblogger
+from instagrapi.exceptions import FeedbackRequired
+
+import sys
 
 
 def unlike_all():
@@ -10,8 +13,16 @@ def unlike_all():
                 joblogger.info(f"Unliked {each_liked_media.id}")
                 print(f"Unlike status: {status}; Media ID: {each_liked_media.id}")
 
+            except FeedbackRequired as e:
+                clientlogger.error(f"Rate limited: {e}")
+                print(
+                    "Rate limit exceeded. Check client.log for details. Terminating program execution"
+                )
+                sys.exit(1)
+
             except Exception as e:
                 clientlogger.error(
                     f"Error: {e}; Unlike media failed: {each_liked_media}"
                 )
-                break
+                print("Unexpected error occured. Terminating program execution")
+                sys.exit(1)
