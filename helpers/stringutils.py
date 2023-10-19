fcec6fc9b4f3925f26e22927bcfc91edb920c0e4
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-10-20 01:16:11 +0530

    update post unliker (backend)

--- helpers/stringutils.py
+++ helpers/stringutils.py
@@ -1,3 +1,6 @@
+from helpers.configutils import importconfig
+
+
 def str_to_bool(args):
     true_list = ["true", 1, "1", "yes"]
     false_list = ["false", 0, "0", "no"]
@@ -10,3 +13,7 @@ def str_to_bool(args):
 
     else:
         return None
+
+
+def get_config(section, key, default):
+    return default if ((q := importconfig((section))) == None) else (q.get(key))
