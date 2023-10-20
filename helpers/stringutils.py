2663e581d2cce6f25cfbf1c2578bab4a7950e0d2
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-10-20 18:10:38 +0530

    add bool handling in str_to_bool() function

--- helpers/stringutils.py
+++ helpers/stringutils.py
@@ -1,14 +1,11 @@
-from helpers.configutils import importconfig
-
-
 def str_to_bool(args):
     true_list = ["true", 1, "1", "yes"]
     false_list = ["false", 0, "0", "no"]
 
-    if args.lower() in true_list:
+    if str(args).lower() in true_list:
         return True
 
-    elif args.lower() in false_list:
+    elif str(args).lower() in false_list:
         return False
 
     else:
