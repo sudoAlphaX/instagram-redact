e6ceced480b8c1be8d8641583bf77573d6e9a33b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-10-24 00:47:29 +0530

    add delay_range as config.ini variable

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import sys
 
 from instagrapi import Client
 
+from helpers.configutils import read_config
 from helpers.instautils import login
 from helpers.logutils import clientlogger as logger
 from helpers.logutils import consolelog
@@ -9,7 +10,10 @@ from helpers.logutils import consolelog
 cl = login(Client())
 
 if cl is not None:
-    cl.delay_range = [1, 3]
+    cl.delay_range = [
+        int(read_config("ratelimit", "min_delay", 1)),  # type: ignore
+        int(read_config("ratelimit", "max_delay", 5)),  # type: ignore
+    ]
 
     consolelog(
         f"Logged in to Instagram as: {(cl.account_info().dict()).get('username')}"
