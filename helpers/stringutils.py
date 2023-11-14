bb6fc07792d11388fe3bffd6df8aab48627d8d6c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2023-11-14 17:04:23 +0530

    str_to_bool returns original value if not matched and bool handling

--- helpers/stringutils.py
+++ helpers/stringutils.py
@@ -1,25 +1,33 @@
-def str_to_bool(args):
+def str_to_bool(args, fallback=False):
     """
-    The str_to_bool function takes a string as an argument and returns the corresponding boolean value, or None.
+    The str_to_bool function takes a string or boolean as an argument and returns the corresponding boolean value,
+    or the original value if fallback is True and the string does not match any of the expected values.
 
     Args:
         args: Pass in the arguments that are passed into the function
+        fallback: If True, the function will return args when it doesn't match any value in the lists. Defaults to False.
 
     Returns:
-        A boolean value
+        A boolean value, original argument value or None
 
     Doc Author:
         Trelent
     """
 
-    true_list = ["true", 1, "1", "yes"]
-    false_list = ["false", 0, "0", "no"]
+    if isinstance(args, bool):
+        return args
+
+    true_list = ["true", "1", "yes", "y"]
+    false_list = ["false", "0", "no", "n"]
 
     if str(args).lower() in true_list:
         return True
 
     elif str(args).lower() in false_list:
         return False
+
+    elif fallback:
+        return args
 
     else:
         return None
